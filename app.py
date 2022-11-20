96334c805493c46b80335723016d74ac0c86526b
İbrahim Can Gençel <ibrahimcangencel@gmail.com>
2022-11-20 15:44:42 +0100

    Past date predictions are added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, status
 from model import HistoryInput, ForecastInput
 import yfinance as yf
 from datetime import date
+import pandas as pd
 
 from cassandra.forecast import ForecastStrategy, forecast
 
@@ -45,6 +46,23 @@ def get_stock_prices(data: ForecastInput):
     df = yf.Ticker(data.stock).history(
         start=data.start_date, end=data.end_date, interval=data.interval
     )
+
     strategy = data.strategy or ForecastStrategy.naive_lstm
     predictions = forecast(data.stock, df, strategy=strategy)
-    return predictions
+
+    # Find time difference between start and end date in hours
+    timediff = (data.end_date - data.start_date)
+    timediff = timediff.days * 24 + timediff.seconds // 3600
+    if timediff >= 24:
+        new_predictions = []
+        for i in range(12):
+            new_df = df.iloc[:-(12-i)]
+            strategy = data.strategy or ForecastStrategy.naive_lstm
+            pred = forecast(data.stock, new_df, strategy=strategy)
+            new_predictions.append(pred['y'][0])
+        comparision_df = (df.reset_index().iloc[-12:].rename(columns={'index': 'Date', 'Close':'Actual'})[['Date', 'Actual']])
+        comparision_df['Predicted'] = new_predictions
+        comparisions = comparision_df.to_dict(orient='records')
+        return predictions, comparisions
+    else:
+        return predictions
