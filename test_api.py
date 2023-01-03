3981143af9b34d952fa79533a15272685595f2e6
ytdmetu <117662775+ytdmetu@users.noreply.github.com>
2023-01-03 22:42:33 +0300

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,29 +1,35 @@
-from app import api
-from starlette.testclient import TestClient
+stocklist=["META","AAPL"]
+strategylist=["gaussian","random_walk","naive_forecast","multivariate_diff"]
 
-test_client = TestClient(api)
-test_client.auth = ('pytest', 'pytest')
+def forecast_test():
+      forecast_endpoint = 'https://cassandra-api.herokuapp.com/forecast'
+      for i in stocklist:
+        for k in strategylist:
+          payload = {
+          
+          "stock": i,
+          "start_date": "2022-11-02",
+          "end_date": "2022-11-09",
+          "interval": "1h",
+          "n_forecast": 12,
+          "strategy": k}
+          res = requests.post(forecast_endpoint, json=payload)
+          res_body = res.json()
+          assert res.status_code == 200
+            
 
-def test_forecast_api():
-    payload = {
-        "stock": "AAPL",
-        "start_date": "2022-11-01",
-        "end_date": "2022-11-02",
-        "interval": "1h",
-        "n_forecast": 10,
-        "strategy": "gaussian"
-    }
-    response = test_client.post("/forecast", json=payload)
-    assert response.status_code == 200
-
-def test_forecast_past_hour_api():
-    payload = {
-        "stock": "AAPL",
-        "start_date": "2022-11-01",
-        "end_date": "2022-11-02",
-        "interval": "1h",
-        "n_forecast": 10,
-        "strategy": "gaussian"
-    }
-    response = test_client.post("/forecast_past_hours", json=payload)
-    assert response.status_code == 200
+def test_forecast_past_hour():
+    forecast_endpoint = 'https://cassandra-api.herokuapp.com/forecast_past_hours'
+    for i in stocklist:
+        for k in strategylist:
+          payload = {
+          "stock": i,
+          "start_date": "2022-11-01",
+          "end_date": "2022-11-02",
+          "interval": "1h",
+          "n_forecast": 10,
+          "strategy": k
+      }
+          res = requests.post(forecast_endpoint, json=payload)
+          res_body = res.json()
+          assert res.status_code == 200
